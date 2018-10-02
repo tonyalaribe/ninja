5e56fdd86019df6d17e12d74d06a2c8bf0e66310
Anthony Alaribe <anthonyalaribe@gmail.com>
2018-10-02 10:20:52 +0100

    create viper config file driven workflow

---
+++ cmd/root.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ninja",
+	Short: "Ninja is a dynamic api engine",
+	Long:  `Ninja lets you build powerful api's(REST, graphql, grpc, etc) for your apps and web applications using a very simple interface.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("main function")
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	var cfgFile string
+	cobra.OnInitialize(initConfig(cfgFile))
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ninja.yaml)")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func initConfig(cfgFile string) func() {
+	return func() {
+		// Don't forget to read config either from cfgFile or from home directory!
+		if cfgFile != "" {
+			// Use config file from the flag.
+			viper.SetConfigFile(cfgFile)
+		} else {
+			// Find home directory.
+			home, err := homedir.Dir()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			// Search config in home directory with name ".cobra" (without extension).
+			viper.AddConfigPath(home)
+			viper.AddConfigPath(".")
+			viper.SetConfigName(".ninja")
+		}
+
+		if err := viper.ReadInConfig(); err != nil {
+			fmt.Println("Unable to read config:", err)
+			os.Exit(1)
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import (
+	"github.com/tonyalaribe/ninja/cmd"
+)
 
+func main() {
+	cmd.Execute()
 }
